fea16326c016f97528624cf29be88c66bf3bacee
mmjmm <patiencendiligence@gmail.com>
2023-01-25 21:31:00 +0900

    fix: minor meta tag fix

--- components/common/Seo.tsx
+++ components/common/Seo.tsx
@@ -12,7 +12,10 @@ const Seo = ({ title }: ISeoProps) => {
         <meta property="og:url" content="https://holywater-lunch.vercel.app" />
         <meta property="og:title" content="HOLY WATER LUNCH" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="../../public/image.png" />
+        <meta
+          property="og:image"
+          content="https://github.com/patiencendiligence/holy-water-lunch/blob/a2b9d699b2fcf1c75520a2a90fbf1f2641600f5e/public/image.png?raw=true"
+        />
         <meta property="og:description" content="성수동 점심 도우미" />
       </Head>
     </>
