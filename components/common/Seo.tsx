a2b9d699b2fcf1c75520a2a90fbf1f2641600f5e
mmjmm <patiencendiligence@gmail.com>
2023-01-25 20:31:23 +0900

    chore: set meta tag

--- components/common/Seo.tsx
+++ components/common/Seo.tsx
@@ -6,9 +6,16 @@ interface ISeoProps {
 
 const Seo = ({ title }: ISeoProps) => {
   return (
-    <Head>
-      <title>{title} | HolyWaterLunch</title>
-    </Head>
+    <>
+      <Head>
+        <title>{title} | HolyWaterLunch</title>
+        <meta property="og:url" content="https://holywater-lunch.vercel.app" />
+        <meta property="og:title" content="HOLY WATER LUNCH" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="../../public/image.png" />
+        <meta property="og:description" content="성수동 점심 도우미" />
+      </Head>
+    </>
   );
 };
 export default Seo;
