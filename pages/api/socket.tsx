ee5c11ba0eaac8e9929adcbc01e0cd5055a649e5
mmjmm <patiencendiligence@gmail.com>
2023-04-12 23:18:37 +0900

    fix: chat
    feat: add types
    
    fix: ui fix

--- pages/api/socket.tsx
+++ pages/api/socket.tsx
@@ -1,43 +1,24 @@
-import { Server, ServerOptions } from "socket.io";
-import messageHandler from "../../utils/sockets/messageHandler";
+import { NextApiRequest } from "next";
+import { NextApiResponseServerIO } from "src/types/next";
+import { Server as ServerIO } from "socket.io";
+import { Server as NetServer } from "http";
 
-export default function SocketHandler(
-  req: any,
-  res: {
-    socket: { server: Partial<ServerOptions> | any | undefined };
-    end: () => void;
-  }
-) {
-  // It means that socket server was already initialised
-  if (res?.socket?.server?.io) {
-    console.log("Already set up");
-    res.end();
-    return;
-  }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-  const io = new Server(res.socket.server);
-  res.socket.server.io = io;
-
-  // Define actions inside
-  io.on("connection", function (socket: any) {
-    messageHandler(io, socket);
-    socket.on("newUserConnect", function (name: any) {
-      socket.name = name;
-
-      socket.broadcast.emit("updateMessage", {
-        name: "SERVER",
-        message: name + "님이 접속했습니다.",
-      });
+export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (!res.socket.server.io) {
+    console.log("New Socket.io server...");
+    // adapt Next's net Server to http Server
+    const httpServer: NetServer = res.socket.server as any;
+    const io = new ServerIO(httpServer, {
+      path: "/api/socketio",
     });
-
-    socket.on("disconnect", function () {
-      socket.broadcast.emit("updateMessage", {
-        name: "SERVER",
-        message: socket.name + "님이 퇴장했습니다.",
-      });
-    });
-  });
-
-  console.log("Setting up socket");
+    // append SocketIO server to Next.js socket server response
+    res.socket.server.io = io;
+  }
   res.end();
-}
+};
