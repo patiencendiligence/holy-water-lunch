2606af5e8a3aba343893e6ee54a7b3625a6dfdcd
mmjmm <patiencendiligence@gmail.com>
2023-04-14 07:29:40 +0900

    test: chat

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,8 +1,9 @@
-import io, { Socket } from "socket.io-client";
-import { useState, useEffect } from "react";
+// import io, { Socket } from "socket.io-client";
+import io from "socket.io-client";
+import { useState, useEffect, use } from "react";
 import { DefaultEventsMap } from "@socket.io/component-emitter";
 
-let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
+// let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 
 type Message = {
   author: string;
@@ -79,37 +80,128 @@ const nameMaker = function () {
     foods[Math.floor(Math.random() * foods.length)]
   }`;
 };
+
+// export default function Chat() {
+//   const [username, setUsername] = useState("");
+//   const [message, setMessage] = useState("");
+//   const [messages, setMessages] = useState<Array<Message>>([]);
+
+//   useEffect(() => {
+//     socket = io();
+//     socket.emit("newUserConnect", username);
+//   }, [username]);
+//   useEffect(() => {
+//     setUsername(nameMaker());
+//     socketInitializer();
+//   }, []);
+
+//   const socketInitializer = async () => {
+//     // We just call it because we don't need anything else out of it
+//     await fetch("/api/socket");
+
+//     socket = io();
+
+//     socket.on("newIncomingMessage", (msg) => {
+//       setMessages((currentMsg) => [
+//         ...currentMsg,
+//         { author: msg.author, message: msg.message },
+//       ]);
+//       console.log(messages);
+//     });
+//   };
+
+//   const sendMessage = async () => {
+//     socket.emit("createdMessage", { author: username, message });
+//     setMessages((currentMsg) => [...currentMsg, { author: username, message }]);
+//     setMessage("");
+//   };
+
+//   const handleKeypress = (e: { keyCode: number }) => {
+//     //it triggers by pressing the enter key
+//     console.log(e.keyCode === 13, "ENTER!");
+//     if (e.keyCode === 13) {
+//       if (message && message !== "") {
+//         sendMessage();
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
+//       <div className="gap-4 flex flex-col items-center justify-center w-full h-full">
+//         <p className="font-bold  text-xl">{username}님, 안녕하세요!👋</p>
+//         <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md text-black ">
+//           <div className="h-full last:border-b-0 overflow-y-scroll">
+//             {messages.map((msg, i) => {
+//               return (
+//                 <div
+//                   className="w-full py-1 px-2 border-b border-gray-200"
+//                   key={i}
+//                 >
+//                   {msg.author} : {msg.message}
+//                 </div>
+//               );
+//             })}
+//           </div>
+//           <div className="border-t border-gray-300 w-full flex rounded-bl-md">
+//             <input
+//               type="text"
+//               placeholder="냠냠..."
+//               value={message}
+//               className="outline-none py-2 px-2 rounded-bl-md flex-1"
+//               onChange={(e) => setMessage(e.target.value)}
+//               onKeyUp={handleKeypress}
+//             />
+//             <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-200 transition-all">
+//               <button
+//                 className=" px-3 h-full"
+//                 onClick={() => {
+//                   sendMessage();
+//                 }}
+//               >
+//                 SEND
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 export default function Chat() {
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<Array<Message>>([]);
 
   useEffect(() => {
-    socket = io();
-    socket.emit("newUserConnect", username);
-  }, [username]);
-  useEffect(() => {
-    setUsername(nameMaker());
-    socketInitializer();
-  }, []);
+    fetch("/api/socketio").finally(() => {
+      const socket = io();
+      socket.on("connect", () => {
+        console.log("connect");
+        setUsername(nameMaker());
+        socket.emit("hello");
+      });
+      socket.emit("createdMessage", { author: username, message });
+      socket.on("hello", (data) => {
+        console.log("hello", data);
+        socket.emit("newUserConnect", username);
+      });
 
-  const socketInitializer = async () => {
-    // We just call it because we don't need anything else out of it
-    await fetch("/api/socket");
+      socket.on("a user connected", () => {
+        console.log("a user connected");
+      });
+      socket.on("disconnect", () => {
+        console.log("disconnect");
+      });
 
-    socket = io();
-
-    socket.on("newIncomingMessage", (msg) => {
-      setMessages((currentMsg) => [
-        ...currentMsg,
-        { author: msg.author, message: msg.message },
-      ]);
-      console.log(messages);
+      socket.on("newIncomingMessage", () => {
+        console.log("disconnect");
+      });
     });
-  };
+  }, []); // Added [] as useEffect filter so it will be executed only once, when component is mounted
 
   const sendMessage = async () => {
-    socket.emit("createdMessage", { author: username, message });
     setMessages((currentMsg) => [...currentMsg, { author: username, message }]);
     setMessage("");
   };
@@ -123,7 +215,6 @@ export default function Chat() {
       }
     }
   };
-
   return (
     <div className="flex items-center p-4 mx-auto min-h-screen justify-center bg-purple-500">
       <div className="gap-4 flex flex-col items-center justify-center w-full h-full">
