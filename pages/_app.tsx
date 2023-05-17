7f6894d3bbfa8129011e761eb4127b98ea558425
mmjmm <patiencendiligence@gmail.com>
2023-05-17 18:13:05 +0900

    feat: fix error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,7 @@ import { ReactElement, ReactNode, useEffect, useState } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import Script from "next/script";
-import * as gtag from "@/lib/gtag";
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
 };
@@ -43,39 +41,10 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
       },
     },
   });
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-        }}
-      />
       {getLayout(<Component {...pageProps} />)}
     </QueryClientProvider>
     //  <QueryProvider pageProps={pageProps}>{getLayout(<Component {...pageProps} />)}</QueryProvider>
